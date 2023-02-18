a9312475b811222f0e13d27e14a9666d5661ab39
adaizjr <adaizjr@126.com>
2023-02-18 10:30:28 +0800

    丹毒增加，炼丹平衡，炼器平衡

---
+++ dan_qi_pingheng/dan_qi_pingheng/Class1.cs
@@ -0,0 +1,77 @@
+﻿using Bag;
+using BepInEx;
+using HarmonyLib;
+using JSONClass;
+using KBEngine;
+using LianQi;
+using script.NewLianDan.LianDan;
+using System;
+using System.Reflection;
+using UnityEngine;
+
+namespace dan_qi_pingheng
+{
+    [BepInPlugin("plugins.zjr.mcsmod", "这是我的第一个BepIn插件", "1.0.0.0")]
+    public class MyFirstBepInExMod : BaseUnityPlugin
+    {// 在插件启动时会直接调用Awake()方法
+        void Awake()
+        {
+            // 使用Debug.Log()方法来将文本输出到控制台
+            Debug.Log("Hello, world!");
+            Harmony.CreateAndPatchAll(Assembly.GetExecutingAssembly());
+        }
+    }
+
+    [HarmonyPatch(typeof(GUIPackage.item), "gongneng")]
+    class DanduPatch
+    {
+        public static void Postfix(GUIPackage.item __instance)
+        {
+            int type = _ItemJsonData.DataDict[__instance.itemID].type;
+            if (type == 5 && !jsonData.instance.ItemJsonData[string.Concat(__instance.itemID)]["seid"].ToList().Contains(31))
+            {
+                KBEngine.Avatar player = PlayerEx.Player;
+                int level = (int)player.level;
+                level = (level + 5) / 3;
+                KBEngine.Avatar avatar = (KBEngine.Avatar)KBEngineApp.app.player();
+                if (__instance.quality > level + 1)
+                {
+                    avatar.AddDandu((__instance.quality - level - 1) * 30);
+                }
+            }
+        }
+    }
+    [HarmonyPatch(typeof(LianDanPanel), "PutDanLu", new Type[] { typeof(DanLuSlot) })]
+    class DanLuPatch
+    {
+        public static bool Prefix(ref DanLuSlot dragSlot)
+        {
+            KBEngine.Avatar player = PlayerEx.Player;
+            int level = (int)player.level;
+            level = (level + 5) / 3;
+            return level + 1 >= dragSlot.Item.GetImgQuality();
+        }
+    }
+    [HarmonyPatch(typeof(LianDanPanel), "PutCaoYao", new Type[] { typeof(LianDanSlot) })]
+    class CaoyaoPatch
+    {
+        public static bool Prefix(ref LianDanSlot dragSlot)
+        {
+            KBEngine.Avatar player = PlayerEx.Player;
+            int level = (int)player.level;
+            level = (level + 5) / 3;
+            return level + 1 >= dragSlot.Item.GetImgQuality();
+        }
+    }
+    [HarmonyPatch(typeof(LianQiTotalManager), "PutItem", new Type[] { typeof(LianQiSlot) })]
+    class DuanzaoPatch
+    {
+        public static bool Prefix(ref LianQiSlot dragSlot)
+        {
+            KBEngine.Avatar player = PlayerEx.Player;
+            int level = (int)player.level;
+            level = (level + 5) / 3;
+            return level + 1 >= dragSlot.Item.GetImgQuality();
+        }
+    }
+}
